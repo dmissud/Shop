e022eb220a2ac99d25277d8406547cd1b52e0967
pascal.desprez <pascal.desprez@fr.ibm.com>
2020-06-26 16:58:43 +0200

    order implementation

--- domain/src/main/java/org/dbs/shop/domain/Item.java
+++ domain/src/main/java/org/dbs/shop/domain/Item.java
@@ -4,10 +4,32 @@ import java.math.BigDecimal;
 
 public class Item {
 
-	private String productName;
+	private final String productName;
 
-	private BigDecimal productPrice;
+	private final BigDecimal productPrice;
 
-	private int quantity;
+	private final int quantity;
+
+	public Item(final String productName, final BigDecimal productPrice, final int quantity) {
+		this.productName = productName;
+		this.productPrice = productPrice;
+		this.quantity = quantity;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public BigDecimal getProductPrice() {
+		return productPrice;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public BigDecimal getTotalCost() {
+		return productPrice.multiply(BigDecimal.valueOf(quantity));
+	}
 
 }
--- domain/src/main/java/org/dbs/shop/domain/Order.java
+++ domain/src/main/java/org/dbs/shop/domain/Order.java
@@ -2,11 +2,16 @@ package org.dbs.shop.domain;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import javax.validation.constraints.NotNull;
 
 public class Order {
 
-	private Customer customer;
+	@NotNull
+	private final Customer customer;
 
 	private final int number;
 
@@ -14,13 +19,36 @@ public class Order {
 
 	private LocalDate shipmentDate;
 
-	private BigDecimal total;
+	private BigDecimal total = BigDecimal.ZERO;
 
-	private List<Item> items;
+	private final List<Item> items;
 
-	public Order(final int number, final LocalDate orderDate) {
+	public Order(final Customer customer, final int number, final LocalDate orderDate) {
+		this.customer = customer;
 		this.number = number;
 		this.orderDate = orderDate;
+		items = new ArrayList<Item>();
+	}
+
+	public void addItem(final Item item) {
+		items.add(item);
+		total = total.add(item.getTotalCost());
+	}
+
+	public BigDecimal getTotal() {
+		return total;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public List<Item> getItems() {
+		return Collections.unmodifiableList(items);
 	}
 
 }
